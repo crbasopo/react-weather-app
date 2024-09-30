a9e9f4779544fe2fb821c22f1e577deefbd66076
crbasopo <crbasopo@gmail.com>
2024-09-30 21:59:37 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Cheryl R Basopo and us{" "}
+        <a
+          href="https://github.com/crbasopo/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
