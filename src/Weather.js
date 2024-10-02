fa60fc73a823d26c39d904bb5f4aca7d0492b7eb
crbasopo <crbasopo@gmail.com>
2024-10-02 10:28:58 +0200

    Changed icons to animated and added a new component

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       city: response.data.city,
       humidity: response.data.temperature.humidity,
       description: response.data.condition.description,
-      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+      icon: response.data.condition.icon,
       date: new Date(response.data.time * 1000),
     });
   }
